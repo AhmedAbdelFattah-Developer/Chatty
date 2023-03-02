95ab833b20138588baabd9fb19367e4817c79f3f
AhmedAbdelFattah-Developer <ahmedabdelfattah.developer@gmail.com>
2023-03-02 14:44:18 +0200

    initial commit

---
+++ lib/common/pages/frame/welcome/controller.dart
@@ -0,0 +1,13 @@
+import 'package:chatty/common/pages/frame/welcome/state.dart';
+import 'package:get/get.dart';
+
+class WelcomeController extends GetxController {
+  WelcomeController();
+  final title = "Chatty .";
+  final state = WelcomeState();
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+}
